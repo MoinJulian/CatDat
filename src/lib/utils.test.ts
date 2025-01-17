e0840f980b9a0608b65009bc3627c6a2aaef3ac2
Script Raccoon <scriptraccoon@gmail.com>
2025-01-17 11:38:43 +0100

    add missing test

--- src/lib/utils.test.ts
+++ src/lib/utils.test.ts
@@ -1,7 +1,14 @@
-import { negate_prefix } from './utils'
+import { properties_dictionary } from './dictionaries/properties'
+import { get_property_url, negate_prefix } from './utils'
 
 describe('negate_prefix', () => {
 	it("negates 'has a' to 'does not have a'", () => {
 		expect(negate_prefix('has a')).toBe('does not have a')
+	})
+})
+
+describe('get_property_url', () => {
+	it("should return '/property/abelian'", () => {
+		expect(get_property_url(properties_dictionary.abelian)).toBe('/property/abelian')
 	})
 })
