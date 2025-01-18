7741175a3b47453bce8e5a02147c9db537cdca82
Script Raccoon <scriptraccoon@gmail.com>
2025-01-18 17:05:08 +0100

    function to retrieve dual implications

---
+++ src/lib/dualization.test.ts
@@ -0,0 +1,82 @@
+import { get_dual_implication, get_new_dual_implication } from './dualization'
+import type { Implication } from './types'
+
+describe('get_dual_implication', () => {
+	it('should return null when an assumption cannot be dualized', () => {
+		const implication: Implication = {
+			assumptions: ['algebraic'],
+			conclusions: ['cocomplete'],
+		}
+		expect(get_dual_implication(implication)).toBeNull()
+	})
+
+	it('should return null when a conclusion cannot be dualized', () => {
+		const implication: Implication = {
+			assumptions: ['algebraic'],
+			conclusions: ['locally finitely presentable'],
+		}
+		expect(get_dual_implication(implication)).toBeNull()
+	})
+
+	it('should return the dual implication if available', () => {
+		const implication: Implication = {
+			assumptions: ['binary products', 'equalizers'],
+			conclusions: ['pullbacks'],
+		}
+		expect(get_dual_implication(implication)).toEqual({
+			assumptions: ['binary coproducts', 'coequalizers'],
+			conclusions: ['pushouts'],
+		})
+	})
+
+	it('should respect equivalences', () => {
+		const implication: Implication = {
+			equivalent: true,
+			assumptions: ['finite coproducts', 'coequalizers'],
+			conclusions: ['finitely cocomplete'],
+		}
+
+		expect(get_dual_implication(implication)).toEqual({
+			equivalent: true,
+			assumptions: ['finite products', 'equalizers'],
+			conclusions: ['finitely complete'],
+		})
+	})
+
+	it('should preserve self-dual implications', () => {
+		const implication: Implication = {
+			assumptions: ['finite'],
+			conclusions: ['small'],
+		}
+		expect(get_dual_implication(implication)).toEqual(implication)
+	})
+})
+
+describe('get_new_dual_implication', () => {
+	it('should return the dual implication if available', () => {
+		const implication: Implication = {
+			assumptions: ['binary products', 'equalizers'],
+			conclusions: ['pullbacks'],
+		}
+		expect(get_new_dual_implication(implication)).toEqual({
+			assumptions: ['binary coproducts', 'coequalizers'],
+			conclusions: ['pushouts'],
+		})
+	})
+
+	it('should return null for a self-dual implication', () => {
+		const implication: Implication = {
+			assumptions: ['abelian'],
+			conclusions: ['pointed'],
+		}
+		expect(get_new_dual_implication(implication)).toBe(null)
+	})
+
+	it('should not preserve self-dual implications', () => {
+		const implication: Implication = {
+			assumptions: ['finite'],
+			conclusions: ['small'],
+		}
+		expect(get_new_dual_implication(implication)).toBe(null)
+	})
+})
---
+++ src/lib/dualization.ts
@@ -0,0 +1,35 @@
+import { properties_dictionary, type PropertyName } from './dictionaries/properties'
+import type { Implication } from './types'
+
+export function get_dual_implication(implication: Implication): Implication | null {
+	const dual_implication: Implication = {
+		assumptions: [],
+		conclusions: [],
+	}
+	for (const assumption of implication.assumptions) {
+		const property = properties_dictionary[assumption]
+		if (!property.dual) return null
+		dual_implication.assumptions.push(property.dual as PropertyName)
+	}
+	for (const conclusion of implication.conclusions) {
+		const property = properties_dictionary[conclusion]
+		if (!property.dual) return null
+		dual_implication.conclusions.push(property.dual as PropertyName)
+	}
+
+	if (implication.equivalent) {
+		dual_implication.equivalent = true
+	}
+
+	return dual_implication
+}
+
+export function get_new_dual_implication(implication: Implication): Implication | null {
+	const dual_implication = get_dual_implication(implication)
+	if (!dual_implication) return null
+	const is_the_same =
+		implication.assumptions.join(',') === dual_implication.assumptions.join(',') &&
+		implication.conclusions.join(',') === dual_implication.conclusions.join(',')
+	if (is_the_same) return null
+	return dual_implication
+}
