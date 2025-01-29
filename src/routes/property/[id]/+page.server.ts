763ffec8192b23f43d653c296b412c8ba6b1148b
Script Raccoon <scriptraccoon@gmail.com>
2025-01-29 14:03:21 +0100

    use search method on property page

--- src/routes/property/[id]/+page.server.ts
+++ src/routes/property/[id]/+page.server.ts
@@ -1,7 +1,7 @@
 import { error } from '@sveltejs/kit'
 import type { PageServerLoad } from './$types'
 
-import { categories_detailed } from '$lib/categories/categories.utils'
+import { category_system } from '$lib/categories/categories.utils'
 import { is_valid_property } from '$lib/properties/propertyIDs'
 import {
 	decode_property_ID,
@@ -11,23 +11,16 @@ import { render_formulas_in_object } from '$lib/rendering'
 
 export const load: PageServerLoad = (event) => {
 	const id = decode_property_ID(event.params.id)
+
 	const valid = is_valid_property(id)
 
 	if (!valid) return error(404, 'Property not found')
 
+	const categories_with_this_property = category_system.search([id], [])
+	const categories_without_this_property = category_system.search([], [id])
+	const unknown_categories = category_system.search([], [], [id])
+
 	const property = properties_dictionary[id]
-
-	const categories_with_this_property = categories_detailed.filter((category) =>
-		category.all_properties.includes(property.id),
-	)
-
-	const categories_without_this_property = categories_detailed.filter((category) =>
-		category.all_non_properties.includes(property.id),
-	)
-
-	const unknown_categories = categories_detailed.filter((category) =>
-		category.unknown_properties.includes(property.id),
-	)
 
 	return {
 		property: render_formulas_in_object(property, ['description']),
