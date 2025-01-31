e6694bbaaedbb90215648abfdf56c582feb200a2
Script Raccoon <scriptraccoon@gmail.com>
2025-01-31 20:36:36 +0100

    display categories with missing info on epis or monos

--- src/routes/missing/+page.server.ts
+++ src/routes/missing/+page.server.ts
@@ -1,10 +1,19 @@
 import type { PageServerLoad } from './$types'
 import { category_system } from '$lib/categories/categories.utils'
+import { categories } from '$lib/categories/categories'
 
 export const load: PageServerLoad = () => {
 	return {
 		missing_basic_combinations: category_system.missing_basic_combinations,
 		categories_with_unknown_properties:
 			category_system.entities_with_unknown_properties,
+		categories_with_unknown_monomorphisms: categories.filter(
+			(category) =>
+				!category.monomorphisms || category.monomorphisms.includes('TODO:'),
+		),
+		categories_with_unknown_epimorphisms: categories.filter(
+			(category) =>
+				!category.epimorphisms || category.epimorphisms.includes('TODO:'),
+		),
 	}
 }
--- src/routes/missing/+page.svelte
+++ src/routes/missing/+page.svelte
@@ -7,21 +7,39 @@
 
 	const { data } = $props()
 
-	const { missing_basic_combinations, categories_with_unknown_properties } = data
+	const {
+		missing_basic_combinations,
+		categories_with_unknown_properties,
+		categories_with_unknown_monomorphisms,
+		categories_with_unknown_epimorphisms,
+	} = data
 </script>
 
 <svelte:head>
 	<title>Missing data</title>
 </svelte:head>
 
-<h2>Categories with unknown properties</h2>
+<h2>Missing data</h2>
+
+This page lists some missing data in the database. Please help us fill in the gaps by
+contributing to the
+<a href="https://github.com/ScriptRaccoon/catdat" target="_blank">GitHub repository</a>.
+
+<h3>Categories with unknown properties</h3>
 
 There are {categories_with_unknown_properties.length} categories that have some unknown properties.
-Please help us fill in the gaps by contributing to this project.
 
 <CategoryList items={categories_with_unknown_properties} />
 
-<h2>Missing property combinations</h2>
+<h3>Categories with unknown monomorphisms</h3>
+
+<CategoryList items={categories_with_unknown_monomorphisms} />
+
+<h3>Categories with unknown epimorphisms</h3>
+
+<CategoryList items={categories_with_unknown_epimorphisms} />
+
+<h3>Missing property combinations</h3>
 
 Among the consistent combinations of the form "property P, but not property Q", the
 following combinations are currently not yet witnessed by an example category in our
