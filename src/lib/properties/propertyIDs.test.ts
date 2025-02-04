6c8aed5bfff267937f3329cdf921a5a43c3748ea
Script Raccoon <scriptraccoon@gmail.com>
2025-02-04 22:26:49 +0100

    defensive test

--- src/lib/properties/propertyIDs.test.ts
+++ src/lib/properties/propertyIDs.test.ts
@@ -1,4 +1,4 @@
-import { is_valid_property } from './propertyIDs'
+import { is_valid_property, PROPERTY_IDs } from './propertyIDs'
 
 describe('is valid property', () => {
 	it("should return true for 'small'", () => {
@@ -6,5 +6,13 @@ describe('is valid property', () => {
 	})
 	it("should return false for 'smol'", () => {
 		expect(is_valid_property('smol')).toBe(false)
+	})
+})
+
+describe('each ID', () => {
+	it('should not contain an underscore (this would mess up the URL encoding)', () => {
+		for (const id of PROPERTY_IDs) {
+			expect(id).not.toContain('_')
+		}
 	})
 })
