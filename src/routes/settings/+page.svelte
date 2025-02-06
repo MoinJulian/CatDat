211955c40b0e4b1cb0e7d30e82500b880dc96316
Script Raccoon <scriptraccoon@gmail.com>
2025-02-06 01:07:00 +0100

    use chips on settings page

--- src/routes/settings/+page.svelte
+++ src/routes/settings/+page.svelte
@@ -1,10 +1,14 @@
 <script lang="ts">
+	import Chip from '$lib/components/Chip.svelte'
+	import ChipGroup from '$lib/components/ChipGroup.svelte'
 	import {
 		category_detail_level,
 		CATEGORY_DETAIL_LEVELS,
 		update_category_detail_level,
 	} from '$lib/settings/detail_level.svelte'
 	import { theme, THEMES, update_theme } from '$lib/settings/theme.svelte'
+	import { faCheckCircle } from '@fortawesome/free-solid-svg-icons'
+	import Fa from 'svelte-fa'
 
 	$effect(() => update_theme(theme.value))
 	$effect(() => update_category_detail_level(category_detail_level.value))
@@ -12,34 +16,57 @@
 
 <h2>Settings</h2>
 
-<section aria-label="Select a theme">
-	Theme:
-	{#each THEMES as option}
-		<label class:selected={theme.value === option}>
-			<input
-				class="visually-hidden"
-				type="radio"
-				name="color"
-				value={option}
-				bind:group={theme.value}
-			/>{option}
-		</label>
-	{/each}
+<section>
+	<h3>Theme</h3>
+
+	<ChipGroup>
+		{#each THEMES as option}
+			{@const selected = theme.value === option}
+			<label class:selected>
+				<input
+					class="visually-hidden"
+					type="radio"
+					name="color"
+					value={option}
+					bind:group={theme.value}
+				/>
+				<Chip>
+					{option}
+					{#if selected}
+						&nbsp;
+						<Fa icon={faCheckCircle} />
+					{/if}
+				</Chip>
+			</label>
+		{/each}
+	</ChipGroup>
 </section>
 
-<section aria-label="Select a category detail level">
-	Category detail level:
-	{#each Object.keys(CATEGORY_DETAIL_LEVELS) as level}
-		<label class:selected={category_detail_level.value === level}>
-			<input
-				class="visually-hidden"
-				type="radio"
-				name="category-detail-level"
-				value={level}
-				bind:group={category_detail_level.value}
-			/>{level}
-		</label>
-	{/each}
+<section>
+	<h3>Category detail level</h3>
+
+	<ChipGroup>
+		{#each Object.keys(CATEGORY_DETAIL_LEVELS) as level}
+			{@const selected = category_detail_level.value === level}
+			<label class:selected>
+				<input
+					class="visually-hidden"
+					type="radio"
+					name="category-detail-level"
+					value={level}
+					bind:group={category_detail_level.value}
+				/>
+				<Chip>
+					{level}
+					{#if selected}
+						&nbsp;
+						<Fa icon={faCheckCircle} />
+					{/if}
+				</Chip>
+			</label>
+		{/each}
+	</ChipGroup>
+
 	<p class="hint" aria-live="polite">
 		{CATEGORY_DETAIL_LEVELS[category_detail_level.value]}
 	</p>
@@ -48,19 +75,10 @@
 <style>
 	label {
 		cursor: pointer;
-		margin-inline: 0.25rem;
 	}
 
-	label.selected {
-		color: var(--accent-color);
-	}
-
-	label:focus-within {
+	label:has(:focus-visible) {
 		outline: 2px solid var(--outline-color);
 		outline-offset: 2px;
-	}
-
-	section {
-		margin-block: 1rem;
 	}
 </style>
