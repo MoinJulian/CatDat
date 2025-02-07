0c365147f02e5db2d0c24e9a2106323088a5462b
Script Raccoon <scriptraccoon@gmail.com>
2025-02-07 09:06:19 +0100

    render implication list with new data

--- src/routes/implications/+page.server.ts
+++ src/routes/implications/+page.server.ts
@@ -1,19 +1,24 @@
 import { render_formulas_in_object } from '$lib/commons/rendering'
-import { implications } from '$lib/implications/implications'
+import { implications, type Implication } from '$lib/data/implications.data'
 import { implications_with_duals } from '$lib/properties/properties.utils'
 import type { PageServerLoad } from './$types'
 
 export const load: PageServerLoad = (event) => {
 	const show_all_implications = event.url.searchParams.has('show_all')
 
-	const implications_to_show = show_all_implications
-		? implications_with_duals
+	// TODO: remove the typecast later
+	const implications_to_show: Implication[] = show_all_implications
+		? (implications_with_duals as Implication[])
 		: implications
 
+	const sorted_implications = implications_to_show.toSorted((a, b) =>
+		a.assumptions[0].localeCompare(b.assumptions[0]),
+	)
+
+	const rendered_implications = sorted_implications.map(render_formulas_in_object)
+
 	return {
-		implications: implications_to_show
-			.toSorted((a, b) => a.assumptions[0].localeCompare(b.assumptions[0]))
-			.map(render_formulas_in_object),
+		implications: rendered_implications,
 		show_all_implications,
 	}
 }
