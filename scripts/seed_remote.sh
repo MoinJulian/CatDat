6cad49e97e949c4bb9e649ebc5c282b41d4098cd
Script Raccoon <scriptraccoon@gmail.com>
2026-03-13 01:17:26 +0100

    add scripts for remote data

---
+++ scripts/seed_remote.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+set -e
+
+for file in database/schema/[0-9][0-9]_*.sql; do
+    echo "Applying schema: $file..."
+    turso db shell catdat < "$file"
+done
+
+for file in database/source_data/*.sql; do
+    echo "Inserting source data from: $file..."
+    turso db shell catdat < "$file"
+done
